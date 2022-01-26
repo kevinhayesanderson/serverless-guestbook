2a99c6da315ea6bfdb8936dc29b976e9eaed2e03
Kevin Hayes Anderson <kevinhayesanderson@gmail.com>
2022-01-26 12:46:58 +0530

    Updated api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d89d5258.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
